694b552c0c9b9150e09a8b34e35ddb8aa7a55c12
Shivam Maurya <vibhamaurya011@gmail.com>
2024-01-02 23:17:27 +0530

    tailwind CSS added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 function App() {
 
   return (
-    <div>
-      MediCare
+    <div className="text-3xl font-bold">
+      <h1 className="text-gray-500">Medicare</h1>
     </div>
   );
 }
