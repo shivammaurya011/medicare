b7e760864b506521c701c646e7ae28003b3ef463
Shivam Maurya <71092987+shivammaurya011@users.noreply.github.com>
2024-01-05 19:36:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="text-3xl center font-bold">
       <h1 className="text-gray-500">Medicare</h1>
-      <p className="text-blue-500">Comming soon....</p>
+      
     </div>
   );
 }
