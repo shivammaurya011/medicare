5eec133ed470004ecb9a1044b0b63642bc440eae
Shivam Maurya <vibhamaurya011@gmail.com>
2024-01-05 19:34:28 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 function App() {
 
   return (
-    <div className="text-3xl font-bold">
+    <div className="text-3xl center font-bold">
       <h1 className="text-gray-500">Medicare</h1>
-      <p>Comming soon....</p>
+      <p className="text-blue-500">Comming soon....</p>
     </div>
   );
 }
