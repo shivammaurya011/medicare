2be9e1b375421d9ab5c29e3a139a7bbfdce1770e
Shivam Maurya <vibhamaurya011@gmail.com>
2024-01-06 20:27:00 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="text-3xl center font-bold">
       <h1 className="text-gray-500">Medicare</h1>
-      
+      <p>Shivam</p>
+      <p>Maurya</p>
     </div>
   );
 }
