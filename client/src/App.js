b9178f47edf9d36a262b29dce75a2d79c734aa16
Shivam Maurya <71092987+shivammaurya011@users.noreply.github.com>
2024-01-09 20:14:41 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="text-3xl center font-bold">
       <h1 className="text-gray-500">Medicare</h1>
-      <p>Shivam Maurya</p>
+      
+    <p>Shivam Maurya</p>
+    <p>Shivam Maurya</p>
       
     </div>
   );
