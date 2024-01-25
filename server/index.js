945f604942ed7fb8a13617b9de8fa66aa4a36d37
Shivam Maurya <71092987+shivammaurya011@users.noreply.github.com>
2024-01-25 16:07:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 const { connection } = require("./config/db")
 require("dotenv").config()
 const port = process.env.PORT
+
+
 const app = express()
 app.use(express.json())
 
