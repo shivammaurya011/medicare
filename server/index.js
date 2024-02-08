5fb40c939ab4df9554b8e6f5fb8212accfb8444b
Shivam Maurya <71092987+shivammaurya011@users.noreply.github.com>
2024-02-08 20:09:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express")
 const { connection } = require("./config/db")
 require("dotenv").config()
 const port = process.env.PORT
-
 const app = express()
 app.use(express.json())
 app.listen(port, async()=>{
